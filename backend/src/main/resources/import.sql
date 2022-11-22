5241ea8ded0dea5360b127c6fca13835d2865dff
Fagner Cruz <fagner@sti.ufpb.br>
2022-11-22 12:04:42 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into tb_category (name) values ('Book');
+insert into tb_category (name) values ('Manga');
+insert into tb_category (name) values ('Electronics');
+insert into tb_category (name) values ('Computer');
+insert into tb_category (name) values ('Smartphone');
