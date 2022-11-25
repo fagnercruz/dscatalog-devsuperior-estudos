1938a4916848a641411b398c6be063fcb707f59f
Fagner Cunha da Cruz <fagnerccruz@gmail.com>
2022-11-25 18:30:19 -0300

    Refactoring Pageable methods

--- backend/src/main/java/br/devsuperior/dscatalog/resources/ProdutcResource.java
+++ backend/src/main/java/br/devsuperior/dscatalog/resources/ProdutcResource.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,16 +30,8 @@ public class ProdutcResource {
 	private ProductService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction
-			){
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction) ,orderBy);
-		
-		Page<ProductDTO> lista = service.findAllPaginado(pageRequest);
-		
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+		Page<ProductDTO> lista = service.findAllPaginado(pageable);
 		return ResponseEntity.ok(lista);
 	}
 	
