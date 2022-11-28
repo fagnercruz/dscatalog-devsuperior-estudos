bdebe20c9327edeb00d34c35e1f1134a6b8614ed
Fagner Cruz <fagner@sti.ufpb.br>
2022-11-28 13:21:17 -0300

    fist product web layer test

---
+++ backend/src/test/java/br/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -0,0 +1,46 @@
+package br.devsuperior.dscatalog.resources;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+import br.devsuperior.dscatalog.dto.ProductDTO;
+import br.devsuperior.dscatalog.services.ProductService;
+
+@WebMvcTest(ProductResource.class)
+public class ProductResourceTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private ProductService service;
+	
+	private ProductDTO dto;
+	private PageImpl<ProductDTO> page;
+	
+	@BeforeEach
+	void setup() throws Exception {
+		dto = new ProductDTO();
+		page = new PageImpl<>(List.of(dto));
+		
+		when(service.findAllPaginado(any())).thenReturn(page);
+	}
+	
+	@Test
+	public void findAllPaginadoShouldReturnAnPage() throws Exception {
+		mockMvc.perform(get("/products")).andExpect(status().is2xxSuccessful());
+	}
+	
+}
