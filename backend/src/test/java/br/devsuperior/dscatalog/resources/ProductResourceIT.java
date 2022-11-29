827a992a5c560dd818de22e2e986a130f3201a53
Fagner Cruz <fagner@sti.ufpb.br>
2022-11-29 09:40:54 -0300

    Finish integration tests

---
+++ backend/src/test/java/br/devsuperior/dscatalog/resources/ProductResourceIT.java
@@ -0,0 +1,93 @@
+package br.devsuperior.dscatalog.resources;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.Instant;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.devsuperior.dscatalog.dto.ProductDTO;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResourceIT {
+
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private Long existingId;
+	private Long noExistingId;
+	private Long countTotalProduct;
+	private ProductDTO dto;
+	
+
+	@BeforeEach
+	void setup() throws Exception {
+		existingId = 2l;
+		noExistingId = 346l;
+		countTotalProduct = 25l;
+		dto = new ProductDTO(null, "Bazuca Militar", "Bazucona bitelosa", 3000.0, "http://www.g1.com.br", Instant.now());
+	}
+	
+	@Test
+	public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+		ResultActions actions = mockMvc.perform(
+				get("/products?page=0&size=12&sort=name,asc")
+					.accept(MediaType.APPLICATION_JSON)
+			);
+		actions.andExpect(status().isOk());
+		actions.andExpect(jsonPath("$.totalElements").value(countTotalProduct));
+		actions.andExpect(jsonPath("$.content").exists());
+		actions.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+		actions.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+		actions.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+		
+	}
+	
+	@Test
+	public void updateShouldReturnObjectDtoWhenIdExist() throws Exception {
+		
+		String JsonBody = objectMapper.writeValueAsString(dto);
+		
+		ResultActions resultActions = mockMvc.perform(
+				put("/products/{id}", existingId)
+					.content(JsonBody)
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON)
+		);
+		resultActions.andExpect(status().isOk());
+		resultActions.andExpect(jsonPath("$.id").value(existingId));
+		resultActions.andExpect(jsonPath("$.name").value("Bazuca Militar"));
+	}
+	
+	@Test
+	public void updateShouldReturnNotFoundnWhenIdDoesntExist() throws Exception {
+		String JsonBody = objectMapper.writeValueAsString(dto);
+		
+		ResultActions resultActions = mockMvc.perform(
+				put("/products/{id}", noExistingId)
+					.content(JsonBody)
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON)
+		);
+		resultActions.andExpect(status().isNotFound());
+	}
+}
--- backend/src/test/java/br/devsuperior/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/br/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -1,14 +1,27 @@
 package br.devsuperior.dscatalog.services;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.devsuperior.dscatalog.dto.ProductDTO;
+import br.devsuperior.dscatalog.repositories.ProductRepository;
+import br.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 	
 	@Autowired
 	private ProductService productService;
+	@Autowired
+	private ProductRepository productRepository;
 	
 	private Long existingId;
 	private Long noExistingId;
@@ -17,6 +30,54 @@ public class ProductServiceIT {
 
 	@BeforeEach
 	void setup() throws Exception {
+		existingId = 2l;
+		noExistingId = 346l;
+		countTotalProduct = 25l;
+	}
+	
+	@Test
+	public void deleteShouldDeleteResourceWhenIdExist() {
+		productService.delete(existingId);
+		
+		Assertions.assertEquals(countTotalProduct-1, productRepository.count());
+	}
+	
+	@Test
+	public void deleteShouldThrowRecourceNotFoundWhenIdDoesntExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			productService.delete(noExistingId);
+		});
+	}
+	
+	@Test
+	public void findAllPaginadoShouldReturnPageWhenPage0AndSize10() {
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		Page<ProductDTO> results = productService.findAllPaginado(pageRequest);
+		
+		Assertions.assertTrue(!results.isEmpty());       						  // testa se a pagina está vazia
+		Assertions.assertEquals(0, results.getNumber()); 						  // testa a página
+		Assertions.assertEquals(10, results.getSize()); 						  // testa a qtde por pagina
+		Assertions.assertEquals(countTotalProduct, results.getTotalElements());   // testa o total de elementos da cosulta
+	}
+	
+	@Test
+	public void findAllPaginadoShouldReturnEmptyPageWhenPageOverflow() {
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		Page<ProductDTO> results = productService.findAllPaginado(pageRequest);
+		
+		Assertions.assertTrue(results.isEmpty());       						  // testa se a pagina está vazia
+		
+	}
+	
+	@Test
+	public void findAllPaginadoShouldReturnSortedPageWhenSortByName() {
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+		Page<ProductDTO> results = productService.findAllPaginado(pageRequest);
+		
+		Assertions.assertTrue(!results.isEmpty());       						  // testa se a pagina está vazia
+		Assertions.assertEquals("Macbook Pro", results.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", results.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", results.getContent().get(2).getName());
 		
 	}
 }
