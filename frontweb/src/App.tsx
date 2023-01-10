58662a2ce2e985dd7537c91710c75b9f0ab47894
Fagner Cruz <fagner@sti.ufpb.br>
2023-01-10 13:50:55 -0300

    navbar css

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -3,7 +3,12 @@ import Navbar from 'components/Navbar';
 import './App.css';
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <h1>DS Catalog ReactJS Application</h1>
+    </>
+  );
 }
 
 export default App;
