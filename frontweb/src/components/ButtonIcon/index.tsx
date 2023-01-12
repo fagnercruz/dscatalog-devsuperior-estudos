fd2099fdf67b9f0b8c6de4e0b35de74673f55d2c
Fagner Cunha da Cruz <fagnerccruz@gmail.com>
2023-01-12 20:55:49 -0300

    adjusts home navigation button

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -1,11 +1,17 @@
 import './styles.css';
+import { ReactComponent as SetaIcon } from 'assets/images/Seta.svg';
 
 const ButtonIcon = () => {
-    return (
-        <button className="btn btn-primary btn-icon">
-            <h6>inicie agora a sua busca</h6>
-        </button>
-    );
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>inicie agora a sua busca</h6>
+      </button>
+      <div className="btn-icon-container">
+        <SetaIcon />
+      </div>
+    </div>
+  );
 };
 
 export default ButtonIcon;
